e354e1b503c93daa4f140db67a4107181787113e
Kelton Finch <keltonfinch@gmail.com>
2014-04-23 20:18:28 -0400

    beep

---
+++ kfinch_Chessbot/src/chess/Coordinate.java
@@ -0,0 +1,21 @@
+package chess;
+
+public class Coordinate{
+	
+	public int x, y;
+	
+	public Coordinate(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public String toString(){
+		return "(" + x + "," + y + ")";
+	}
+	
+	public boolean equals(Object o){
+		Coordinate c = (Coordinate) o;
+		return ((x == c.x) && (y == c.y));
+	}
+	
+}
